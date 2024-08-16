c3a5e1c9e9d67bca206d5e1e39b1fd3d1f248634
Sachin Chanduveetil <sachisavera@gmail.com>
2024-08-16 12:37:06 +0530

    API Call Code Demo

---
+++ React-APICalls/src/App.tsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from 'react'
+import './App.css';
+import Loader from './components/Loader'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm';
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [error, setError] = useState({})
+
+  interface Todo {
+    title: string;
+    id: number;
+    completed: boolean;
+  }
+
+  const handleCompleted = (index: number) => {
+    const newTodos = [...todos]
+    newTodos[index].completed = !newTodos[index].completed
+    setTodos(newTodos)
+  }
+
+  const handleDelete =(index: number) => {
+    const newTodos = [...todos]
+    newTodos.splice(index,1)
+    setTodos(newTodos)
+  }
+
+  const handleOnSubmit = (value: string) => {
+    setTodos([...todos, {title: value, id: todos.length +1, completed: false }])
+  }
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(res => setTodos(res.slice(0,10)))
+    .catch(error => setError(error))
+  }, [])
+  return (
+    <div className="App">
+      {todos.length > 0 ? todos.map((todos:Todo, index: number) => <Todo todo={todos} index={index} handleCompleted={handleCompleted} handleDelete={handleDelete}/>) : (<Loader/>)}
+      <TodoForm handleOnSubmit={handleOnSubmit} />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-APICalls/src/components/Todo.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {TodoType} from '../types/Todo.types';
+
+interface Props {
+    todo: TodoType
+    index:number;
+    handleCompleted: (id: number) => void;
+    handleDelete: (id: number) => void;
+}
+
+const Todo:React.FC<Props> = ({todo, index, handleCompleted, handleDelete}) => {
+    return(
+        <div className='todo' style={{textDecoration: todo.completed ? 'line-through' : ''}}>
+            {todo.title}
+            <div>
+                <button onClick={() => handleCompleted(index)}>{todo.completed ? 'Incomplete': 'Complete'}</button>
+                <button onClick={() => handleDelete(index)}>Trash</button>
+            </div>
+        </div>
+    )
+}
+export default Todo
